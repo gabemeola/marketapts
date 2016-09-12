6c7ab4791f2b699208d8a038f685940708e7f61c
gabemeola <gabemeola@gmail.com>
2016-09-12 01:50:09 -0600

    added scrollTo util function

--- app/containers/Main/MainContainer.jsx
+++ app/containers/Main/MainContainer.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { Main } from 'components';
+import scrollTo from 'utils/scrollTo';
 
 class MainContainer extends Component {
+	componentDidUpdate() {
+		scrollTo(window.innerHeight);
+	}
 	render() {
 		return(
 			<Main
